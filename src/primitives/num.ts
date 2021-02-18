732d534f88a0e1816070246832caaba3ff525c25
Maxim Yakovlev <max.yakovlev555@gmail.com>
2021-02-18 10:33:44 +0300

    Revert "test: split files on folders, add full tests for num.ts"
    This reverts commit 6ec14046

--- src/primitives/num.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '@src/index';
-
-export const num = (config: Config<{ min: number; max: number; type?: 'float'|'int'; }>) =>
-{
-    if (config.nullable)
-        if (trueOrFalse) return null
-    const rand = Math.random() * (config.max - config.min) + config.min
-    return config.type === 'float' ? rand : Math.round(rand)
-}
